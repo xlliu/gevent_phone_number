95376aab16cd7aa8adbe6e19de691d6dece40da4
xlliu <liu.xuelong@163.com>
2017-01-13 04:50:17 +0000

    s

--- hit_the_lib.py
+++ hit_the_lib.py
@@ -5,6 +5,10 @@ import gevent.monkey
 
 gevent.monkey.patch_all()
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 import pandas as pd
 import random
 
@@ -29,21 +33,22 @@ class HitTheLibrary(object):
     def __run(self):
         with pd.ExcelFile('test.xlsx') as xls:
             df1 = pd.read_excel(xls, '10w')
-            tasks = [gevent.spawn(self.__core, str(tel_num)) for tel_num in xrange(10)]
-            # tasks = (gevent.spawn(self.__core, str(tel_num)) for tel_num in df1[u'手机号码'])
-            
-            gevent.joinall(tasks, timeout=2)
-            for greenlet in tasks:
-                print "结束停止切没有异常: %s" %(greenlet.successful() if greenlet.successful() else "结果是空的")
-                print "结束停止: %s" %(greenlet.ready() if greenlet.ready() else "结果是空的")
-                print "没被捕获的异常: %s" %greenlet.exception
-            
-            print '============================='
-            gevent.sleep(2)
-            for greenlet in tasks:
-                print "结束停止切没有异常: %s" %(greenlet.successful() if greenlet.successful() else "结果是空的")
-                print "结束停止: %s" %(greenlet.ready() if greenlet.ready() else "结果是空的")
-                print "没被捕获的异常: %s" %greenlet.exception
-            # print self.count
+        print df1
+        tasks = [gevent.spawn(self.__core, str(tel_num)) for tel_num in df1[u'电话号码']]
+        # tasks = (gevent.spawn(self.__core, str(tel_num)) for tel_num in df1[u'手机号码'])
+        
+        gevent.joinall(tasks, timeout=2, raise_error=False)
+        for greenlet in tasks:
+            print "结束停止切没有异常: %s" %(greenlet.successful() if greenlet.successful() else False)
+            print "结束停止: %s" %(greenlet.ready() if greenlet.ready() else False)
+            print "没被捕获的异常: %s" %greenlet.exception
+        
+        print '============================='
+        gevent.sleep(2)
+        for greenlet in tasks:
+            print "结束停止切没有异常: %s" %(greenlet.successful() if greenlet.successful() else False)
+            print "结束停止: %s" %(greenlet.ready() if greenlet.ready() else False)
+            print "没被捕获的异常: %s" %greenlet.exception
+        # print self.count
 
-HitTheLibrary()
+
