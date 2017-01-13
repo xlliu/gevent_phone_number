d6a237768dbac5e72ed14fd2db0a7f55b4c80357
xlliu <liu.xuelong@163.com>
2017-01-13 06:11:07 +0000

    s

--- hit_the_lib.py
+++ hit_the_lib.py
@@ -11,6 +11,7 @@ sys.setdefaultencoding('utf-8')
 
 import pandas as pd
 import random
+import requests
 
 
 class HitTheLibrary(object):
@@ -21,19 +22,21 @@ class HitTheLibrary(object):
 
     def __init__(self):
         super(HitTheLibrary, self).__init__()
-        self.count = 0
+        self.session = requests.session()
+        self.__sites = []
         self.__run()
 
     def __core(self, tel_num):
-        gevent.sleep(3)
-        self.count += 1
+        # gevent.sleep(3)  
         print('Running in tel_num: %s' %tel_num)
+        
+        getattr()
         # print('Explicit context switch to foo again')
 
     def __run(self):
         with pd.ExcelFile('test.xlsx') as xls:
             df1 = pd.read_excel(xls, '10w')
-        print df1
+        # print df1
         tasks = [gevent.spawn(self.__core, str(tel_num)) for tel_num in df1[u'电话号码']]
         # tasks = (gevent.spawn(self.__core, str(tel_num)) for tel_num in df1[u'手机号码'])
         
@@ -50,5 +53,4 @@ class HitTheLibrary(object):
             print "结束停止: %s" %(greenlet.ready() if greenlet.ready() else False)
             print "没被捕获的异常: %s" %greenlet.exception
         # print self.count
-
 
